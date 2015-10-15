2d39b4f3cf125fa3d309e0c6571b71ab1bf36b3f
sircapel26 <edwincapel26@gmail.com>
2015-10-15 02:11:17 -0700

    added line that references an instace of MainWindowController

--- PasswordGenerator/AppDelegate.swift
+++ PasswordGenerator/AppDelegate.swift
@@ -10,6 +10,8 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var mainWindowController: MainWindowController?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
