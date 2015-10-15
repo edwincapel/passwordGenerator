5ee3a8246623186ea82203662b568bd409a77b6c
sircapel26 <edwincapel26@gmail.com>
2015-10-15 01:11:57 -0700

    initial osx bas code

---
+++ PasswordGenerator/PasswordGenerator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PasswordGenerator
+//
+//  Created by Edwin Capel on 10/15/15.
+//  Copyright © 2015 Edwin Capel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
