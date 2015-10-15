8b62fca736dfae1f603855b4cc46333d8c34c678
sircapel26 <edwincapel26@gmail.com>
2015-10-15 01:30:14 -0700

    added MainWindowController

---
+++ PasswordGenerator/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  PasswordGenerator
+//
+//  Created by Edwin Capel on 10/15/15.
+//  Copyright © 2015 Edwin Capel. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
