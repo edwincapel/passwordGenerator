152d0da496847463074ed95feef1b449a1a488a1
sircapel26 <edwincapel26@gmail.com>
2015-10-15 02:20:03 -0700

    Created an instance of MainWindowController and show its window

--- PasswordGenerator/AppDelegate.swift
+++ PasswordGenerator/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        
+        let mainWindowController = MainWindowController(windowNibName: "MainWindowController")
+        
+        self.mainWindowController = mainWindowController
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
